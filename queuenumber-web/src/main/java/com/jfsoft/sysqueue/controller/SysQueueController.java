79ea03fcf75ff5b481c887efceb4b69eb59c45b7
wanggang0302 <wanggangjava@126.com>
2017-09-11 20:10:49 +0800

    队列设置

--- queuenumber-web/src/main/java/com/jfsoft/sysqueue/controller/SysQueueController.java
+++ queuenumber-web/src/main/java/com/jfsoft/sysqueue/controller/SysQueueController.java
@@ -13,6 +13,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -83,6 +84,15 @@ public class SysQueueController extends BaseController {
                 SerializerFeature.WriteNullStringAsEmpty);
 
         return resultJson;
+    }
+
+    /**
+     * 跳转到新增页面
+     */
+    @RequestMapping(value = "/toAdd", method = RequestMethod.GET)
+    public String toAdd(HttpSession session, Model model) {
+
+        return "/sysqueue/add";
     }
 
     /**
